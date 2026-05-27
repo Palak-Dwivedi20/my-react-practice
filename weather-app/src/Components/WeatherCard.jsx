792930729156cf1db3c514d43616684edf2b3bbc
Palak Dwivedi <dwivedipalak23@gmail.com>
2026-05-27 23:32:51 +0530

    enhance weather app with dynamic ui and weather icons

--- weather-app/src/Components/WeatherCard.jsx
+++ weather-app/src/Components/WeatherCard.jsx
@@ -1,34 +1,56 @@
 import "./WeatherCard.css";
 
+import { FaSun ,  FaCloud, FaCloudRain } from "react-icons/fa";
+
 export default function WeatherCard({
     weather
 }) {
 
+    let weatherType = weather.current_condition[0].weatherDesc[0].value;
+
+    let cardClass = weatherType.includes("Sunny") ? "sunny" 
+    : weatherType.includes("Cloud") ? "cloudy" : "rainy";
+
     return (
 
-        <div className="weather-card">
+        <div className={`weather-card ${cardClass}`}>
 
-            <p>
-                Temp:
-                {
-                    weather.current_condition[0].temp_C
-                }°C
-            </p>
+            <div className="weather-icon">
 
-            <p>
-                Wind:
                 {
-                    weather.current_condition[0].windspeedKmph
-                } km/h
-            </p>
-
-            <p>
-                Description:
-                {
-                    weather.current_condition[0]
-                    .weatherDesc[0].value
+                    weatherType.includes("Sunny") 
+                    ? <FaSun /> 
+                    : weatherType.includes("Cloud") 
+                    ? <FaCloud />
+                    : <FaCloudRain />
                 }
-            </p>
+
+            </div>
+
+            <h2>{weatherType}</h2>
+
+            <p>
+                Temp: {weather.current_condition[0].temp_C}°C
+                </p>
+                
+                <p>
+                    Humidity: {weather.current_condition[0].humidity}%
+                    </p>
+
+                    <p>
+                        Feels Like:
+                        {weather.current_condition[0].FeelsLikeC}°C
+                        </p>
+                    
+                    <p>
+                        Wind: {weather.current_condition[0].windspeedKmph} km/h
+                        </p>
+
+                        
+                        <p>
+                            Description:
+                            {weather.current_condition[0].weatherDesc[0].value}
+                            </p>
 
         </div>
     );
--- weather-app/src/Components/WeatherSearch.jsx
+++ weather-app/src/Components/WeatherSearch.jsx
@@ -1,11 +1,19 @@
 import "./SearchBox.css";
 
 export default function WeatherSearch({search, setSearch, getData}) {
+
+    let handleOnKeyDown = (e) => {
+        if(e.key === "Enter") {
+            getData();
+        }
+    }
+
     return (
         <div className="search-box ">
             <input type="text" 
             placeholder="Enter City"
             value={search} 
+            onKeyDown={handleOnKeyDown}
             onChange={(e) => setSearch(e.target.value)} />
 
             <button onClick={getData}>Search</button>
